2953c2920c04eced2721e1e6edfbd7b247e7beb8
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-09 19:10:45 -0600

    completed challenge 2. defined a callback and passed it to getWeather to handle updating the dom elements

--- challenge1.js
+++ challenge1.js
@@ -1,12 +1,11 @@
 
-function getWeather(zip, apiKey) {
+function getWeather(zip, apiKey, callback) {
     const units = 'imperial'
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
     fetch(path)
       .then(res => res.json())
       .then(json => {
-        tempEl.innerHTML = json.main.temp
-        descEl.innerHTML = json.weather[0].description
+        callback(json)
       })
       .catch(err => console.log(err.message))
   }
