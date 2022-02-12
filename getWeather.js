27a08079a514c615656d9cd0fe1bcc22cf976fe9
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-11 19:09:07 -0600

    added functions for get weather based on city and geolocation to getweather.js

--- getWeather.js
+++ getWeather.js
@@ -9,9 +9,32 @@
 //     })
     
 //   }
+const url = "https://api.openweathermap.org/data/2.5/weather?"
+const getWeatherZip = async (zip, apiKey, unit="imperial") => {
+    const path = `${url}zip=${zip}&appid=${apiKey}&units=${unit}`
+    const res = await fetch(path)
+    const jsonData = await res.json()
+    const resolvedJsonData = {
+        "temp" : jsonData.main.temp,
+        "desc" : jsonData.weather[0].description,
+        "unit" : unit
+    }
+    return resolvedJsonData 
+}
 
-const getWeather = async (zip, apiKey, unit="imperial") => {
-    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`
+const getWeatherCity = async (city, apiKey, unit="imperial") => {
+    const path = `${url}q=${city}&appid=${apiKey}&units=${unit}`
+    const res = await fetch(path)
+    const jsonData = await res.json()
+    const resolvedJsonData = {
+        "temp" : jsonData.main.temp,
+        "desc" : jsonData.weather[0].description,
+        "unit" : unit
+    }
+    return resolvedJsonData 
+}
+const getWeatherGeo = async (lat, lon, apiKey, unit="imperial") => {
+    const path = `${url}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`
     const res = await fetch(path)
     const jsonData = await res.json()
     const resolvedJsonData = {
