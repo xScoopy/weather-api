c684b7d3abefa0ae3ad03c2d64eb3dc52dada6df
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-11 18:46:30 -0600

    completed challenge 5 with implementing async and await

--- getWeather.js
+++ getWeather.js
@@ -14,5 +14,13 @@ const getWeather = async (zip, apiKey) => {
     const units = 'imperial'
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
     const res = await fetch(path)
-    return (await res.json())
+    const jsonData = await res.json()
+    const resolvedJsonData = {
+        "temp" : jsonData.main.temp,
+        "desc" : jsonData.weather[0].description
+    }
+    return resolvedJsonData 
 }
+
+// tempEl.innerHTML = data.main.temp
+// descEl.innerHTML = data.weather[0].description
