7c7fe43a93e991930094af53c83433416478f58f
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-11 20:27:25 -0600

    Used autoformatter on both index and js files

--- getWeather.js
+++ getWeather.js
@@ -1,27 +1,26 @@
-
-const url = "https://api.openweathermap.org/data/2.5/weather?"
+const url = "https://api.openweathermap.org/data/2.5/weather?";
 
 const apiHelper = async (path) => {
-    const res = await fetch(path)
-    const jsonData = await res.json()
-    const resolvedJsonData = {
-        "temp" : jsonData.main.temp,
-        "desc" : jsonData.weather[0].description,
-        "unit" : unit
-    }
-    return resolvedJsonData 
-}
+  const res = await fetch(path);
+  const jsonData = await res.json();
+  const resolvedJsonData = {
+    temp: jsonData.main.temp,
+    desc: jsonData.weather[0].description,
+    unit: unit,
+  };
+  return resolvedJsonData;
+};
 
-const getWeatherZip = (zip, apiKey, unit="imperial") => {
-    const path = `${url}zip=${zip}&appid=${apiKey}&units=${unit}`
-    return apiHelper(path)
-}
+const getWeatherZip = (zip, apiKey, unit = "imperial") => {
+  const path = `${url}zip=${zip}&appid=${apiKey}&units=${unit}`;
+  return apiHelper(path);
+};
 
-const getWeatherCity = async (city, apiKey, unit="imperial") => {
-    const path = `${url}q=${city}&appid=${apiKey}&units=${unit}`
-    return apiHelper(path)
-}
-const getWeatherGeo = async (lat, lon, apiKey, unit="imperial") => {
-    const path = `${url}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`
-    return apiHelper(path)
-}
+const getWeatherCity = async (city, apiKey, unit = "imperial") => {
+  const path = `${url}q=${city}&appid=${apiKey}&units=${unit}`;
+  return apiHelper(path);
+};
+const getWeatherGeo = async (lat, lon, apiKey, unit = "imperial") => {
+  const path = `${url}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+  return apiHelper(path);
+};
